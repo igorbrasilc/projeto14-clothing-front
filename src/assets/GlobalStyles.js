48db4e43ae3dd34f9ab3f5625cfa49f0a41a0863
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-05-13 11:38:35 -0300

    fix: basic structure

--- src/assets/GlobalStyles.js
+++ src/assets/GlobalStyles.js
@@ -1,5 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  transition: 300ms;
+  --color-thema: #FF233D;
+  --font: 'Roboto', sans-serif;
+}
+
+body {
+  background-color: #FAFAFA;
+}
+`;
 
 export default GlobalStyle;
